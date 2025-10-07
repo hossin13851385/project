dcaa5dbfca007b2a96c309d7ca87076e996dbf35
karchon <u1385hossein1385u@gmail.com>
2025-10-07 22:54:07 +0330

    fix urls for about

--- shop/urls.py
+++ shop/urls.py
@@ -19,5 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('client.urls'))
+    path('' , include('client.urls')),
+    path('about' , include('about.urls'))
 ]
