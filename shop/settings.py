260064615c84b2dee5f440479cf58b41c0fb6a1d
karchon <u1385hossein1385u@gmail.com>
2025-10-08 23:52:39 +0330

    delet about.html

--- shop/settings.py
+++ shop/settings.py
@@ -47,6 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'about',
+    'client',
+    'profiles',
 ]
 
 ROOT_URLCONF = 'shop.urls'
